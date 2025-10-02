da65b03769980960092f82215eb2e5d70a1a15bf
lawrence2moremoholo-byte <lawrence2.moremoholo@gmail.com>
2025-10-02 07:15:52 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from supabase import create_client
 from backend.config import SUPABASE_URL, SUPABASE_KEY
 
 # Create Supabase client from environment variables
-supabase = create_client(https://zgennqckgzokxnfwpsxh.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZW5ucWNrZ3pva3huZndwc3hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzcyMDMsImV4cCI6MjA3NDk1MzIwM30.HAou28iXuAGgGg6gqJF4AowNtSJjNOoBsHqFi6OAURc)
+supabase = create_client("https://zgennqckgzokxnfwpsxh.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZW5ucWNrZ3pva3huZndwc3hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzcyMDMsImV4cCI6MjA3NDk1MzIwM30.HAou28iXuAGgGg6gqJF4AowNtSJjNOoBsHqFi6OAURc")
 
 def insert_review(review_data: dict):
     supabase.table("reviews").insert(review_data).execute()
