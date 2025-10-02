10ce6456528a42bac74882b8a6aef6abd7bb5a3a
lawrence2moremoholo-byte <lawrence2.moremoholo@gmail.com>
2025-10-02 06:12:30 +0200

    Update scraper_google.py

--- backend/scraper_google.py
+++ backend/scraper_google.py
@@ -3,7 +3,7 @@ from database import insert_review
 from config import GOOGLE_API_KEY
 
 def get_google_reviews(place_id):
-    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=reviews&key={GOOGLE_API_KEY}"
+    url = f"https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=reviews&key={AIzaSyBkIAZJwSbbDokzP3lpvfU2Ya2qKOjX_Oo}"
     response = requests.get(url).json()
     reviews = response.get("result", {}).get("reviews", [])
     for rev in reviews:
