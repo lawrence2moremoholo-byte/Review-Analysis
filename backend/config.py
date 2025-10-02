80f1039861279e3082ca49cdd8454591f58b2b14
lawrence2moremoholo-byte <lawrence2.moremoholo@gmail.com>
2025-10-02 07:23:23 +0200

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -8,5 +8,4 @@ SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhY
 GOOGLE_API_KEY = os.getenv("AIzaSyBkIAZJwSbbDokzP3lpvfU2Ya2qKOjX_Oo")
 
 # OpenAI
-OPENAI_API_KEY = os.getenv("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
-")
+OPENAI_API_KEY = os.getenv("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")
