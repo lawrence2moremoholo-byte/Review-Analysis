d790406d75b184a46b12c14e448b70b43618ce9d
lawrence2moremoholo-byte <lawrence2.moremoholo@gmail.com>
2025-10-02 06:42:34 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from supabase import create_client
 from config import SUPABASE_URL, SUPABASE_KEY
 
+# Create client using ENV vars (not hardcoded strings)
 supabase = create_client(https://zgennqckgzokxnfwpsxh.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZW5ucWNrZ3pva3huZndwc3hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzcyMDMsImV4cCI6MjA3NDk1MzIwM30.HAou28iXuAGgGg6gqJF4AowNtSJjNOoBsHqFi6OAURc)
 
 def insert_review(review_data: dict):
@@ -11,4 +12,3 @@ def update_review(review_id, update_data: dict):
 
 def get_all_reviews():
     return supabase.table("reviews").select("*").execute().data
-
